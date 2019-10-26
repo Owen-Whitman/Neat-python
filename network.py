8395741bc16fbb8a4a706c6d897330f7bc13ebef
owenthefuturew <owen@whitmans.us>
2019-10-26 10:36:38 -0400

    innital commit

---
+++ network.py
@@ -0,0 +1,5 @@
+import node_connection_genes
+
+class network():
+    def __init__(self):
+        pass
---
+++ node_connection_genes.py
@@ -0,0 +1,7 @@
+class nodegenes():
+    def __init__(self):
+        pass
+
+class connectiongenes():
+    def __init__(self):
+        pass
